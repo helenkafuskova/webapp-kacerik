59e329986632966cc42db732c843ed341382268c
helenkafuskova <163534848+helenkafuskova@users.noreply.github.com>
2025-09-21 13:42:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 document.getElementById("btn").addEventListener("click", () => {
   document.getElementById("output").innerText = "Stlačil si tlačidlo!";
 });
+
+document.getElementById("btn2").addEventListener("click", () => {
+  document.body.style.backgroundColor =
+    document.body.style.backgroundColor === "lightblue" ? "white" : "lightblue";
+});
+
+document.getElementById("btn3").addEventListener("click", () => {
+  const now = new Date();
+  document.getElementById("time").innerText = "Aktuálny čas: " + now.toLocaleTimeString();
+});
+
+document.getElementById("btn4").addEventListener("click", () => {
+  const name = document.getElementById("nameInput").value;
+  document.getElementById("greeting").innerText =
+    name.trim() === "" ? "Prosím, zadaj svoje meno." : `Ahoj, ${name}! Vitaj na mojej stránke.`;
+});
+
+document.getElementById("btn5").addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
+
